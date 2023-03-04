67a6ed2485282434218c5c3021138b0f9670fd04
Pulkit Methaniya <79719013+Pulkit2099@users.noreply.github.com>
2023-03-05 02:11:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ setInterval(() => {
     hrotation = 30*htime + mtime/2;
     mrotation = 6*mtime;
     srotation = 6*stime;
-
+//h
     hour.style.transform = `rotate(${hrotation}deg)`;
     minute.style.transform = `rotate(${mrotation}deg)`;
     second.style.transform = `rotate(${srotation}deg)`;
